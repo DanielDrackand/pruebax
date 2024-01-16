4000cd868b425b2a3dcbe0ffc567ce641bb1c6a2
Lapp Lucas Daniel <daniellapp2010@gmail.com>
2024-01-16 19:18:42 -0300

    refactorizando logicPlant

--- data/js/getHtml/getsHtml.js
+++ data/js/getHtml/getsHtml.js
@@ -14,6 +14,9 @@ const getsHtml = {
 //imgs of planet
 const imgs = ['data/img/tierra.jpeg','data/img/mercurio.jpeg','data/img/venus.jpeg','data/img/jupiter.jpeg','data/img/marte.jpeg','data/img/saturno.jpeg','data/img/urano.jpeg','data/img/neptuno.jpeg',]
 //Planets
-const namePlan = [ 'Tierra','Mercurio','Venus','Júpiter','Marte','Saturno','Urano','Neptuno'
-]
-export {getsHtml,imgs,namePlan}
+const namePlan = ['Tierra','Mercurio','Venus','Júpiter','Marte','Saturno','Urano','Neptuno'
+]//fijarme si puedo hacerlo array asociativo 
+
+//peso de los planetas 
+const pesoPlanet = [1 , 3.7  * 1 / 9.81 , 8.87 * 1 / 9.81 ,24.79 * 1 / 9.81 , 3.72 * 1 / 9.81 ,10.44 * 1 / 9.81,8.69 * 1 / 9.81,11.15 * 1 / 9.81]
+export {getsHtml,imgs,namePlan,pesoPlanet}
--- data/js/logicPlant/logicPlant.js
+++ data/js/logicPlant/logicPlant.js
@@ -1,12 +1,12 @@
 //getHtml lo necesitamos para cosas concretas
-import {getsHtml,imgs, namePlan} from './../getHtml/getsHtml.js';
+import {getsHtml,imgs,namePlan,pesoPlanet} from './../getHtml/getsHtml.js';
 
 //usamos 'x' para saber que img de planeta mostrar
 //se pude solucionar con un array de planetas
 function namePlanet(x){
-    getsHtml.name_planet.innerHTML = '<p>' + namePlan[x] +'<p>';
+    getsHtml.name_planet.innerHTML = '<p>' + namePlan[x] +'</p>';
 }
-////funciones de bts
+//funciones de bts
 function next(){
     if(getsHtml.x < 7 ){
         getsHtml.x++
@@ -24,35 +24,10 @@ function back(){
 //calculo 
 function calculo(){
     if(getsHtml.pesoUsuario.value == 0){
-        getsHtml.result.innerHTML = '<p> por favor escribe un número</p>';
-    }else{
-        switch(getsHtml.x){ 
-            case 0  :
-                getsHtml.result.innerHTML = '<p> tu peso en la Tierra es ' + getsHtml.pesoUsuario.value + " kg" ;
-            break;  
-            case 1  :
-                getsHtml.result.innerHTML = '<p> tu peso en Mercurio sería ' + parseInt( getsHtml.pesoUsuario.value * 3.7  * 1 / 9.81) + " kg";
-            break; 
-            case 2 :
-                getsHtml.result.innerHTML = '<p> tu peso en Venus sería ' + parseInt(getsHtml.pesoUsuario.value * 8.87 * 1 / 9.81) + " kg";
-            break; 
-            case 3 :
-                getsHtml.result.innerHTML = '<p> tu peso en Júpiter sería ' + parseInt(getsHtml.pesoUsuario.value * 24.79 * 1 / 9.81) + " kg";
-            break; 
-            case 4 :
-                getsHtml.result.innerHTML = '<p> tu peso en Marte sería ' + parseInt(getsHtml.pesoUsuario.value * 3.72 * 1 / 9.81) + " kg";
-            break; 
-            case 5 :
-                getsHtml.result.innerHTML = '<p> tu peso en Saturno sería ' + parseInt(getsHtml.pesoUsuario.value * 10.44 * 1 / 9.81) + " kg";
-            break; 
-            case 6 :
-                getsHtml.result.innerHTML = '<p> tu peso en  Urano  sería ' + parseInt(getsHtml.pesoUsuario.value * 8.69 * 1 / 9.81) + " kg";
-            break; 
-            case 7 :
-            getsHtml.result.innerHTML = '<p> tu peso en Neptuno  sería ' + parseInt(getsHtml.pesoUsuario.value * 11.15 * 1 / 9.81) + " kg";
-            break;
-    }
-           
+        getsHtml.result.innerHTML = '<p> Por favor escribe un número .</p>';
+    }else{   
+        let cal =  getsHtml.pesoUsuario.value * pesoPlanet[getsHtml.x]; 
+        getsHtml.result.innerHTML = "<p> Tu peso en el planeta " + namePlan[getsHtml.x] + " sería: "+parseFloat(cal.toFixed(2))+" kg </p>";
     }
 }
 
